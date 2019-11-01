fc86c82d84ea36e2775d60a138df95bea06c1cfd
Maxim Zhiburt <zhiburt@gmail.com>
2019-11-01 18:51:02 +0300

    Refactoring: move generathion stuff to itself file generator.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod lexer;
 mod parser;
+mod generator;
 
 pub use lexer::{TokenType, Token, Lexer};
 pub use parser::{
     Program, Declaration, Statement, Expression,
     Term, Factor, FactOp, UnaryOp, BinOp, Result
 };
+pub use generator::gen;
