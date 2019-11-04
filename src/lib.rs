f47324683fa7ebe4979dcb505ca7da9f3562c7b6
Maxim Zhiburt <zhiburt@gmail.com>
2019-11-04 19:17:51 +0300

    Change import logic of parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 mod generator;
 mod lexer;
-mod parser;
+pub mod parser;
 
 pub use generator::gen;
 pub use lexer::{Lexer, Token, TokenType};
-pub use parser::{
-    BinOp, Declaration, Expression, FactOp, Factor, Program, Result, Statement, Term, UnaryOp,
-};
