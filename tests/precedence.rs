08ab9f951e7d4ae52c59a01dcbbb4751c7745f7f
Maxim Zhiburt <zhiburt@gmail.com>
2019-11-13 00:46:54 +0300

    Add a bunch of tests

--- tests/precedence.rs
+++ tests/precedence.rs
@@ -1,10 +1,9 @@
 #[test]
 fn add_operation() {
-    compile_expr("13");
+    assert_eq!(10, compile_expr("return 10;"));
+    assert_eq!(40, compile_expr("return (10 + 10) * 2;"));
+    assert_eq!(1, compile_expr("return 10 * 2 && 1 + 1;"));
 }
-
-#[test]
-fn muv_operation() {}
 
 fn compile_expr(expr: &str) -> usize {
     use std::io::Write;
@@ -12,16 +11,37 @@ fn compile_expr(expr: &str) -> usize {
     let mut code_file = std::env::temp_dir();
     code_file.push("code.c");
     let mut file = std::fs::File::create(&code_file).unwrap();
-    let code = format!("int main(){{ return {}; }}", expr);
+    let code = format!("int main(){{ {} }}", expr);
     file.write_all(code.as_bytes()).unwrap();
 
-    let compiler_status = std::process::Command::new("./target/debug/simple-c-compiler")
+    let compiler = std::process::Command::new("./target/debug/simple-c-compiler")
         .arg(&code_file)
         .output()
         .expect("start compilation process");
-    if !compiler_status.status.success() {
+    if !compiler.status.success() {
         println!("{:?}", code_file);
         panic!();
     }
-    0
+
+    let gcc = std::process::Command::new("gcc")
+        .args(&["-m64", "-o", "a.out", "assembly.s"])
+        .spawn()
+        .expect("Run gcc to compile asm")
+        .wait()
+        .expect("Waiting for gcc");
+    if !gcc.success() {
+        println!("{:?}", std::env::current_dir());
+        panic!();
+    }
+
+    let program = std::process::Command::new("./a.out").spawn()
+        .expect("Run compiled programm")
+        .wait()
+        .expect("Waiting for programm");
+
+    std::fs::remove_file("assembly.s").unwrap();
+    std::fs::remove_file(code_file).unwrap();
+    std::fs::remove_file("a.out").unwrap();
+
+    program.code().unwrap() as usize
 }
