68809ec6b703f7681a50729551760fc1c1040977
Maxim Zhiburt <zhiburt@gmail.com>
2020-01-12 18:34:24 +0300

    Restructure tests by removing all mods(it was notices that in such case `cargo test does not run them`)

--- tests/precedence.rs
+++ tests/precedence.rs
@@ -1,83 +1,28 @@
+mod compare;
+use compare::gcc;
+
 #[test]
 fn add_operation() {
-    assert_eq!(40, compile_expr("return (10 + 10) * 2;"));
-    assert_eq!(21, compile_expr("return 10 * 2 + 1;"));
-    assert_eq!(21, compile_expr("return 1 + 10 * 2;"));
+    assert_eq!(40, gcc::compile_expr("(10 + 10) * 2;"));
+    assert_eq!(21, gcc::compile_expr("10 * 2 + 1;"));
+    assert_eq!(21, gcc::compile_expr("1 + 10 * 2;"));
 }
 
 #[test]
 fn minus_operation() {
-    assert_eq!(2, compile_expr("return (10 - 9) * 2;"));
-    assert_eq!(19, compile_expr("return 10 * 2 - 1;"));
-    assert_eq!(4, compile_expr("return 10 - 3 * 2;"));
+    assert_eq!(2, gcc::compile_expr("(10 - 9) * 2;"));
+    assert_eq!(19, gcc::compile_expr("10 * 2 - 1;"));
+    assert_eq!(4, gcc::compile_expr("10 - 3 * 2;"));
 }
 
 #[test]
 fn mul_operation() {
-    assert_eq!(4, compile_expr("return 20 / 10 * 2;"));
-    assert_eq!(24, compile_expr("return 10 * 2 +  2 * 2;"));
+    assert_eq!(4, gcc::compile_expr("20 / 10 * 2;"));
+    assert_eq!(24, gcc::compile_expr("10 * 2 +  2 * 2;"));
 }
 
 #[test]
 fn div_operation() {
-    assert_eq!(1, compile_expr("return 20 / 10 / 2;"));
-    assert_eq!(9, compile_expr("return (10 / 2) +  2 * 2;"));
-}
-
-
-fn compile_expr(expr: &str) -> usize {
-    use std::io::Write;
-
-    let code = format!("int main(){{ {} }}", expr);
-
-    let code_file = random_name("code_", ".c");
-    let mut file = std::fs::File::create(&code_file).unwrap();
-    file.write_all(code.as_bytes()).unwrap();
-
-    let asm_file = random_name("asm_", ".s");
-    let bin_file = random_name("bin_", ".out");
-
-
-    let compiler = std::process::Command::new("./target/debug/simple-c-compiler")
-        .arg(&code_file)
-        .arg("-o")
-        .arg(&asm_file)
-        .output()
-        .expect("start compilation process");
-    if !compiler.status.success() {
-        println!("{:?}", code_file);
-        panic!();
-    }
-
-    let gcc = std::process::Command::new("gcc")
-        .args(&["-m64", "-o", &bin_file, &asm_file])
-        .output()
-        .expect("Run gcc to compile asm")
-        .status;
-
-    if !gcc.success() {
-        println!("{:?}", std::env::current_dir());
-        panic!();
-    }
-
-    let program = std::process::Command::new(format!("./{}", bin_file))
-        .output()
-        .expect("Run compiled programm")
-        .status;
-
-    std::fs::remove_file(code_file).unwrap();
-    std::fs::remove_file(asm_file).unwrap();
-    std::fs::remove_file(bin_file).unwrap();
-
-    program.code().unwrap() as usize
-}
-
-fn random_name(prefix: &str, suffix: &str) -> String {
-    lazy_static::lazy_static!{
-        static ref INDEX: std::sync::Mutex<usize> = std::sync::Mutex::new(0);
-    }
-    let mut i = INDEX.lock().unwrap();
-    *i += 1;
-
-    format!("{}{}{}", prefix, i, suffix)
+    assert_eq!(1, gcc::compile_expr("20 / 10 / 2;"));
+    assert_eq!(9, gcc::compile_expr("(10 / 2) +  2 * 2;"));
 }
--- tests/program.rs
+++ tests/program.rs
@@ -1,210 +1,195 @@
-mod compare_gcc {
-    use super::*;
+mod compare;
+use compare::gcc;
 
-    mod statements {
-        use super::*;
+#[test]
+fn if_statement() {
+    gcc::compare_expr(r"
+        int a = 10;
+        if (a > 0) return 10;
+        else
+            return 20;
+    ");
 
-        #[test]
-        fn if_statement() {
-            compare_expr(r"
-                int a = 10;
-                if (a > 0) return 10;
+    gcc::compare_expr(r"
+        int a = 10;
+        if (a > 0) return 10;
+        return 20;
+    ");
+
+    gcc::compare_expr(r"
+        int a = 10;
+        if (a > 0) return 10;
+    ");
+
+    gcc::compare_expr(r"
+        int a = 10;
+        if (a > 0) { a = 20; }
+        return 20;
+    ");
+
+    gcc::compare_expr(r"
+        int a = -1;
+        if (a > 0) {
+            a = 20;
+        } else if (a < 0) {
+            a = -10;
+        } else {
+            a = 0;
+        }
+
+        return a;
+    ");
+
+    gcc::compare_expr(r"
+        if (1) {
+            return 201;
+        } else if (0) {
+            return 10;
+        } else {
+        }
+    ");
+}
+
+#[test]
+fn while_statement() {
+    gcc::compare_expr(r"
+        int i = 0;
+        while(i < 10) {
+            i++;
+
+        return i;
+    ");
+
+    gcc::compare_expr(r"
+        int i = 0;
+        while(i < 10) i++;
+
+        return i;
+    ");
+
+    gcc::compare_expr(r"
+        int sum = 0;
+        int i = 0;
+        while(sum < 100) {
+            int i = 0;
+            while(i < 10) {
+                i++;
+            }
+            sum += i;
+        }
+
+        return i;
+    ");
+
+    gcc::compare_expr(r"
+        int sum = 0;
+        int i = 0;
+        while(i++ < 10) {
+            sum += i;
+        }
+        return i;
+    ");
+
+    gcc::compare_expr(r"
+        int sum = 0;
+        int i = 0;
+        while(1) {
+            if(i == 10)
+                break;
+            sum += i++;
+        }
+        return i;
+    ");
+}
+
+#[test]
+fn for_statement() {
+    gcc::compare_expr(r"
+        int sum = 0;
+        for(int i = 0; i < 10; i++)
+            sum++;
+        return sum;
+    ");
+
+    gcc::compare_expr(r"
+        int sum = 0;
+        for(int i = 0; i < 10; i++){
+            sum++;
+        }
+        return sum;
+    ");
+
+    gcc::compare_expr(r"
+        int sum = 0;
+        for(int i = 0; i < 10; i++)
+            for(int i = 0; i < 10; i++)
+                sum += i;
+        return sum;
+    ");
+
+    gcc::compare_expr(r"
+        int sum = 0;
+        int i = 2;
+        for(int i = 0; i < 10; i++){
+            int i = 11;
+            sum += i;
+        }
+        return sum;
+    ");
+
+    gcc::compare_expr(r"
+        int i;
+        for(i = 0; i < 10; i++)
+            ;
+        return i;
+    ");
+
+    gcc::compare_expr(r"
+        int i = 0;
+        for(;i < 10;)
+            i++;            
+        return i;
+    ");
+
+    gcc::compare_expr(r"
+        int i = 0;
+        for(;;)
+            if(i < 10)
+                i++;
+            else
+                break;        
+        return i;
+    ");
+}
+
+#[test]
+fn continue_statement() {
+    gcc::compare_expr(r"
+        int sum = 0;
+        for(int i = 0; i < 10; i++)
+            if(i % 2 == 0)
+                sum += i;
+            else
+                continue;
+        return sum;
+    ");
+
+    gcc::compare_expr(r"
+        int sum = 0;
+        for(int i = 1; i < 10; i++)
+            for(int j = 1; j < 10; j++)
+                if(j % i == 0)
+                    sum += j + i;
                 else
-                    return 20;
-            ");
+                    continue;
+        return sum;
+    ");
+}
 
-            compare_expr(r"
-                int a = 10;
-                if (a > 0) return 10;
-                return 20;
-            ");
-
-            compare_expr(r"
-                int a = 10;
-                if (a > 0) return 10;
-            ");
-
-            compare_expr(r"
-                int a = 10;
-                if (a > 0) { a = 20; }
-                return 20;
-            ");
-
-            compare_expr(r"
-                int a = -1;
-                if (a > 0) {
-                    a = 20;
-                } else if (a < 0) {
-                    a = -10;
-                } else {
-                    a = 0;
-                }
-
-                return a;
-            ");
-
-            compare_expr(r"
-                if (1) {
-                    return 201;
-                } else if (0) {
-                    return 10;
-                } else {
-                }
-            ");
-        }
-
-        #[test]
-        fn while_statement() {
-            compare_expr(r"
-                int i = 0;
-                while(i < 10) {
-                    i++;
-                }
-
-                return i;
-            ");
-
-            compare_expr(r"
-                int i = 0;
-                while(i < 10) i++;
-
-                return i;
-            ");
-
-            compare_expr(r"
-                int sum = 0;
-                int i = 0;
-                while(sum < 100) {
-                    int i = 0;
-                    while(i < 10) {
-                        i++;
-                    }
-                    sum += i;
-                }
-
-                return i;
-            ");
-
-            compare_expr(r"
-                int sum = 0;
-                int i = 0;
-                while(i++ < 10) {
-                    sum += i;
-                }
-
-                return i;
-            ");
-
-            compare_expr(r"
-                int sum = 0;
-                int i = 0;
-                while(1) {
-                    if(i == 10)
-                        break;
-
-                    sum += i++;
-                }
-
-                return i;
-            ");
-        }
-
-        #[test]
-        fn for_statement() {
-            compare_expr(r"
-                int sum = 0;
-                for(int i = 0; i < 10; i++)
-                    sum++;
-
-                return sum;
-            ");
-
-            compare_expr(r"
-                int sum = 0;
-                for(int i = 0; i < 10; i++){
-                    sum++;
-                }
-
-                return sum;
-            ");
-
-            compare_expr(r"
-                int sum = 0;
-                for(int i = 0; i < 10; i++)
-                    for(int i = 0; i < 10; i++)
-                        sum += i;
-
-                return sum;
-            ");
-
-            compare_expr(r"
-                int sum = 0;
-                int i = 2;
-                for(int i = 0; i < 10; i++){
-                    int i = 11;
-                    sum += i;
-                }
-
-                return sum;
-            ");
-
-            compare_expr(r"
-                int i;
-                for(i = 0; i < 10; i++)
-                    ;
-                
-                return i;
-            ");
-
-            compare_expr(r"
-                int i = 0;
-                for(;i < 10;)
-                    i++;
-                
-                return i;
-            ");
-
-            compare_expr(r"
-                int i = 0;
-                for(;;)
-                    if(i < 10)
-                        i++;
-                    else
-                        break;
-                
-                return i;
-            ");
-        }
-
-        #[test]
-        fn continue_statement() {
-            compare_expr(r"
-                int sum = 0;
-                for(int i = 0; i < 10; i++)
-                    if(i % 2 == 0)
-                        sum += i;
-                    else
-                        continue;
-
-                return sum;
-            ");
-
-            compare_expr(r"
-                int sum = 0;
-                for(int i = 1; i < 10; i++)
-                    for(int j = 1; j < 10; j++)
-                        if(j % i == 0)
-                            sum += j + i;
-                        else
-                            continue;
-
-                return sum;
-            ");
-        }
-
-        #[test]
-        fn break_statement() {
-            compare_expr(r"
+#[test]
+fn break_statement() {
+    gcc::compare_expr(
+        r"
                 int sum = 0;
                 for(int i = 0; i < 10; i++)
                     if(i % 2 == 0)
@@ -213,200 +198,99 @@ mod compare_gcc {
                         break;
 
                 return sum;
-            ");
+            ",
+    );
 
-            compare_expr(r"
-                int sum = 0;
-                for(int i = 1; i < 10; i++)
-                    for(int j = 1; j < 10; j++)
-                        if(j % i == 0)
-                            sum += j + i;
-                        else
-                            break;
-
-                return sum;
-            ");
-        }
-    }
-
-    mod functions {
-        use super::*;
-
-        #[test]
-        fn simple_fn() {
-            compare_code(r"
-                int add(int a, int b) {
-                    return a + b;
-                }
-
-                int main() {
-                    add(31, 12);
-                }
-            ");
-        }
-
-        #[test]
-        fn decl_fn() {
-            compare_code(r"
-                int add(int a, int b);
-
-                int main() {
-                    add(31, 12);
-                }
-
-                int add(int a, int b) {
-                    return a + b;
-                }
-            ");
-
-            compare_code(r"
-                int add(int a, int b) {
-                    return a + b;
-                }
-
-                int main() {
-                    add(31, 12);
-                }
-
-                int add(int a, int b);
-            ");
-        }
-            
-        #[test]
-        fn recursive() {
-            compare_code(r"
-                int fib(int n) {
-                    if (n == 0 || n == 1) {
-                        return n;
-                    } else {
-                        return fib(n - 1) + fib(n - 2);
-                    }
-                }
-
-                int main() {
-                    int n = 6;
-                    return fib(n);
-                }
-            ");
-        }
-
-        #[test]
-        fn libc_call() {
-            compare_code(r"
-                int putchar(int c);
-
-                int main() {
-                    putchar(72);
-                    putchar(101);
-                    putchar(108);
-                    putchar(108);
-                    putchar(111);
-                    putchar(44);
-                    putchar(32);
-                    putchar(87);
-                    putchar(111);
-                    putchar(114);
-                    putchar(108);
-                    putchar(100);
-                    putchar(33);
-                    putchar(10);
-                }
-            ");
-        }
-    }
+    gcc::compare_expr(r"
+        int sum = 0;
+        for(int i = 1; i < 10; i++)
+            for(int j = 1; j < 10; j++)
+                if(j % i == 0)
+                    sum += j + i;
+                else
+                    break;
+        return sum;
+    ");
 }
 
-fn compare_code(code: &str) {
-    assert_eq!(compile_gcc_expr(&code), compile_expr(&code));
+#[test]
+fn simple_fn() {
+    gcc::compare_code(r"
+        int add(int a, int b) {
+            return a + b;
+        }
+
+        int main() {
+            add(31, 12);
+        }
+    ");
 }
 
-fn compare_expr(expr: &str) {
-    let code = format!("int main(){{ {} }}", expr);
-    assert_eq!(compile_gcc_expr(&code), compile_expr(&code));
+#[test]
+fn decl_fn() {
+    gcc::compare_code(r"
+        int add(int a, int b);
+        
+        int main() {
+            add(31, 12);
+        }
+
+        int add(int a, int b) {
+            return a + b;
+        }
+    ");
+
+    gcc::compare_code(r"
+        int add(int a, int b) {
+            return a + b;
+        }
+
+        int main() {
+            add(31, 12);
+        }
+
+        int add(int a, int b);
+    ");
 }
 
-fn compile_expr(code: &str) -> usize {
-    use std::io::Write;
+#[test]
+fn recursive() {
+    gcc::compare_code(r"
+        int fib(int n) {
+            if (n == 0 || n == 1) {
+                return n;
+            } else {
+                return fib(n - 1) + fib(n - 2);
+            }
+        }
 
-    let code_file = random_name("code_", ".c");
-    let mut file = std::fs::File::create(&code_file).unwrap();
-    file.write_all(code.as_bytes()).unwrap();
-
-    let asm_file = random_name("asm_", ".s");
-    let bin_file = random_name("bin_", ".out");
-
-
-    let compiler = std::process::Command::new("./target/debug/simple-c-compiler")
-        .arg(&code_file)
-        .arg("-o")
-        .arg(&asm_file)
-        .output()
-        .expect("start compilation process");
-    if !compiler.status.success() {
-        println!("{:?}", code_file);
-        panic!();
-    }
-
-    let gcc = std::process::Command::new("gcc")
-        .args(&["-m64", "-o", &bin_file, &asm_file])
-        .output()
-        .expect("Run gcc to compile asm")
-        .status;
-
-    if !gcc.success() {
-        println!("{:?}", std::env::current_dir());
-        panic!();
-    }
-
-    let program = std::process::Command::new(format!("./{}", bin_file))
-        .output()
-        .expect("Run compiled programm")
-        .status;
-
-    std::fs::remove_file(code_file).unwrap();
-    std::fs::remove_file(asm_file).unwrap();
-    std::fs::remove_file(bin_file).unwrap();
-
-    program.code().unwrap() as usize
+        int main() {
+            int n = 6;
+            return fib(n);
+        }
+    ");
 }
 
-fn compile_gcc_expr(code: &str) -> usize {
-    use std::io::Write;
-
-    let code_file = random_name("code_", ".c");
-    let mut file = std::fs::File::create(&code_file).unwrap();
-    file.write_all(code.as_bytes()).unwrap();
-
-    let bin_file = random_name("bin_", ".out");
-
-    let gcc = std::process::Command::new("gcc")
-        .args(&["-m64", "-o", &bin_file, &code_file])
-        .output()
-        .expect("Run gcc to compile asm")
-        .status;
-
-    if !gcc.success() {
-        println!("{:?}", std::env::current_dir());
-        panic!();
-    }
-
-    let program = std::process::Command::new(format!("./{}", bin_file))
-        .output()
-        .expect("Run compiled programm")
-        .status;
-
-    std::fs::remove_file(code_file).unwrap();
-    std::fs::remove_file(bin_file).unwrap();
-
-    program.code().unwrap() as usize
-}
-
-fn random_name(prefix: &str, suffix: &str) -> String {
-    lazy_static::lazy_static!{
-        static ref INDEX: std::sync::Mutex<usize> = std::sync::Mutex::new(0);
-    }
-    let mut i = INDEX.lock().unwrap();
-    *i += 1;
-
-    format!("{}{}{}", prefix, i, suffix)
+#[test]
+fn libc_call() {
+    gcc::compare_code(r"
+        int putchar(int c);
+        
+        int main() {
+            putchar(72);
+            putchar(101);
+            putchar(108);
+            putchar(108);
+            putchar(111);
+            putchar(44);
+            putchar(32);
+            putchar(87);
+            putchar(111);
+            putchar(114);
+            putchar(108);
+            putchar(100);
+            putchar(33);
+            putchar(10);
+        }
+    ");
 }
