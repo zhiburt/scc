4986c4a8b66c5bbff16d0a14f97b746ba6792387
Maxim Zhiburt <zhiburt@gmail.com>
2020-02-17 15:41:05 +0300

    cli: add conditional flags for displaing AST and TAC

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,26 @@ use simple_c_compiler::{checks, generator, il::tac, lexer::Lexer, parser};
 mod pretty_output;
 
 #[derive(Clap)]
-#[clap(name = "scc", version = "0.0.1", about = "simple c compiler")]
-struct Opt {
-    #[clap(short = "v", parse(from_occurrences))]
-    verbose: i32,
+#[clap(
+    name = "scc",
+    version = "0.0.2",
+    about = "
+        A handcrafted compiler of programs written in C to assembler language.
 
+        To start use it pass the file name as an argument and then
+        the result of compilation appears in asm.s file by default.
+
+        It uses GASM syntax for Assembly.
+    ",
+    author = "Maxim Z."
+)]
+struct Opt {
+    #[clap(short = "ast", long = "pretty-ast")]
+    pretty_ast: bool,
+    #[clap(short = "tac", long = "pretty-tac")]
+    pretty_tac: bool,
     #[clap(parse(from_os_str))]
     input_file: PathBuf,
-
     #[clap(short = "o", parse(from_os_str))]
     out_file: Option<PathBuf>,
 }
@@ -28,34 +40,30 @@ fn main() {
     let program = std::fs::File::open(input_file).unwrap();
     let lexer = Lexer::new();
     let tokens = lexer.lex(program);
-    let program = parser::parse(tokens).expect("Cannot parse program");
+    let ast = parser::parse(tokens).expect("Cannot parse program");
 
-    {
-        if opt.verbose > 0 {
-            println!("\n{}\n", pretty_output::pretty_prog(&program));
-        }
-        if opt.verbose > 1 {
-            let tac = tac::il(&program);
-            for f in &tac {
-                pretty_output::pretty_tac(std::io::stdout(), f);
-            }
+    if opt.pretty_ast {
+        println!("\n{}", pretty_output::pretty_prog(&ast));
+    }
 
+    let tac = tac::il(&ast);
+    if opt.pretty_tac {
+        for f in &tac {
             println!();
-
-            if !checks::function_checks::func_check(&program) {
-                eprintln!("invalid function declaration or definition");
-                std::process::exit(120);
-            }
-
-            let mut asm_file = std::fs::File::create(output_file).expect("Cannot create assembler code");
-            
-            let mut generator = generator::from_tac::Transit::new(generator::x64_translator::X64Backend::new());
-            for f in tac {
-                writeln!(asm_file,
-                    "{}",
-                    generator.gen(f)
-                ).unwrap();
-            }
+            pretty_output::pretty_tac(std::io::stdout(), f);
+            println!();
         }
+    }
+
+    if !checks::function_checks::func_check(&ast) {
+        eprintln!("invalid function declaration or definition");
+        std::process::exit(120);
+    }
+
+    let mut asm_file = std::fs::File::create(output_file).expect("Cannot create output file");
+    let mut generator =
+        generator::from_tac::Transit::new(generator::x64_translator::X64Backend::new());
+    for f in tac {
+        writeln!(asm_file, "{}", generator.gen(f)).unwrap();
     }
 }
