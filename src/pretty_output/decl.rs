140c2145cf4600fbb093cdf40752ab0f6c5fdc49
Maxim Zhiburt <zhiburt@gmail.com>
2020-04-29 20:38:50 +0300

    pretty_output: refactoring by using default Visitor.
    The code might got be slightly worse since
    we have to save the state of traversal in a new created structure.
    But I hope it will be better, as we introduce any default methods in visitor itself.
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/pretty_output/decl.rs
+++ src/pretty_output/decl.rs
@@ -1,211 +1,226 @@
-use simple_c_compiler::ast;
-use simple_c_compiler::parser;
+use simple_c_compiler::ast::{BlockItem, Declaration, Exp, FuncDecl, Program, Statement, Visitor};
 
-pub fn pretty_prog(prog: &ast::Program) -> String {
+pub fn pretty_prog(prog: &Program) -> String {
+    let mut printer = Printer::new(0);
+
     let mut out = Vec::new();
     for func in &prog.0 {
-        out.push(pretty_func(func));
+        out.push(printer.function(func));
     }
 
     out.join("\n\n")
 }
 
-pub fn pretty_func(
-    ast::FuncDecl {
-        name,
-        parameters,
-        blocks,
-    }: &ast::FuncDecl,
-) -> String {
-    let params = parameters
-        .iter()
-        .map(|p| format!("INT {}", p))
-        .collect::<Vec<String>>()
-        .join(", ");
+struct Printer {
+    buf: String,
+    ident: usize,
+    lines: Vec<String>,
+}
 
-    let body = blocks.as_ref().map_or("".to_owned(), |blocks| {
-        blocks
+impl Printer {
+    fn new(ident: usize) -> Self {
+        Self {
+            buf: String::new(),
+            ident,
+            lines: Vec::new(),
+        }
+    }
+
+    fn tab<F: FnMut(&mut Printer)>(&mut self, mut f: F) {
+        self.ident += 2;
+        f(self);
+        self.ident -= 2;
+    }
+
+    fn save(&mut self, s: String) {
+        self.buf = s;
+    }
+
+    fn line(&mut self, s: &str) {
+        self.lines.push(str::repeat(" ", self.ident) + s);
+    }
+
+    fn expr(&mut self, exp: &Exp) -> String {
+        Visitor::visit_expr(self, exp);
+        self.buf.clone()
+    }
+
+    fn decl(&mut self, decl: &Declaration) -> String {
+        Visitor::visit_decl(self, decl);
+        self.buf.clone()
+    }
+
+    fn function(&mut self, func: &FuncDecl) -> String {
+        self.visit_function(func);
+        let body = self.lines.join("\n");
+
+        let params = func
+            .parameters
             .iter()
-            .map(|block| {
-                pretty_block(block)
-                    .iter()
-                    .map(|(line, deep)| format!("\t{1}{}", &line, str::repeat("\t", *deep)))
-                    .collect::<Vec<_>>()
-            })
-            .flatten()
+            .map(|p| format!("INT {}", p))
             .collect::<Vec<String>>()
-            .join("\n")
-    });
+            .join(", ");
 
-    format!(
-        "FUNCTION {}:\n\tparameters: {}\n\tbody:\n{}",
-        name, params, body
-    )
-}
-
-fn pretty_block(block: &ast::BlockItem) -> Vec<(String, usize)> {
-    match block {
-        ast::BlockItem::Declaration(decl) => vec![(pretty_decl(decl), 0)],
-        ast::BlockItem::Statement(statement) => pretty_stat(statement, 0),
+        format!(
+            "FUNCTION {}:\n  parameters: {}\n  body:\n{}",
+            func.name, params, body
+        )
     }
 }
 
-fn pretty_stat(st: &ast::Statement, deep: usize) -> Vec<(String, usize)> {
-    match st {
-        ast::Statement::Return { exp } => vec![(format!("RETURN {}", pretty_expr(exp)), deep)],
-        ast::Statement::Exp { exp } => vec![(format!("{}", pretty_opt_expr(exp)), deep)],
-        ast::Statement::Conditional {
-            cond_expr,
-            if_block,
-            else_block,
-        } => {
-            let mut out = Vec::new();
-            out.push((format!("IF {}:", pretty_expr(cond_expr)), deep));
-            out.extend(pretty_stat(if_block, deep + 1));
-
-            if let Some(else_block) = else_block {
-                out.push(("ELSE:".to_string(), deep));
-                out.extend(pretty_stat(else_block, deep + 1));
+impl<'a> Visitor<'a> for Printer {
+    fn visit_expr(&mut self, exp: &'a Exp) {
+        match exp {
+            Exp::BinOp(op, exp1, exp2) => {
+                let left = self.expr(exp1);
+                let right = self.expr(exp2);
+                self.save(format!("{} BIN_OP<{:?}> {}", left, op, right));
             }
-
-            out.push(("END".to_string(), deep));
-
-            out
+            Exp::Const(c) => self.save(format!("{:?}", c)),
+            Exp::UnOp(op, exp) => {
+                let exp = self.expr(exp);
+                self.save(format!("UN_OP<{:?}> {}", op, exp));
+            }
+            Exp::IncOrDec(name, op) => self.save(format!("VAR[{}] {:?}", name, op)),
+            Exp::Assign(name, exp) => {
+                let exp = self.expr(exp);
+                self.save(format!("VAR[{}] = {}", name, exp));
+            }
+            Exp::Var(name) => self.save(format!("VAR[{}]", name)),
+            Exp::AssignOp(name, op, exp) => {
+                let exp = self.expr(exp);
+                self.save(format!("VAR[{}] ASSIGN_OP<{:?}> {}", name, op, exp));
+            }
+            Exp::CondExp(cond, exp1, exp2) => {
+                let cond = self.expr(cond);
+                let exp1 = self.expr(exp1);
+                let exp2 = self.expr(exp2);
+                self.save(format!(
+                    "TERNAR_OP IF {} DO {} ELSE DO {}",
+                    cond, exp1, exp2
+                ));
+            }
+            Exp::FuncCall(name, params) => {
+                let mut f = |e| self.expr(e);
+                let params = params
+                    .iter()
+                    .map(|e| f(e))
+                    .collect::<Vec<String>>()
+                    .join(", ");
+                self.save(format!("CALL {} WITH {}", name, params,));
+            }
         }
-        ast::Statement::Compound { list } => {
-            match list {
-                Some(list) => {
-                    let mut out = Vec::new();
-                    // out.push(("COMPOUND:".to_string(), deep));
-                    for state_or_def in list {
-                        match state_or_def {
-                            ast::BlockItem::Declaration(decl) => {
-                                out.push((pretty_decl(decl), deep))
-                            }
-                            ast::BlockItem::Statement(stat) => out.extend(pretty_stat(stat, deep)),
-                        }
-                    }
-                    out
+    }
+
+    fn visit_statement(&mut self, st: &'a Statement) {
+        match st {
+            Statement::Return { exp } => {
+                let exp = self.expr(exp);
+                self.line(&&format!("RETURN {}", exp));
+            }
+            Statement::Exp { exp } => {
+                let exp = exp.as_ref().map_or("None".to_owned(), |exp| self.expr(exp));
+                self.line(&exp);
+            }
+            Statement::Conditional {
+                cond_expr,
+                if_block,
+                else_block,
+            } => {
+                let cond_expr = self.expr(cond_expr);
+                self.line(&format!("IF {}:", cond_expr));
+
+                self.tab(|s| s.visit_statement(if_block));
+
+                if let Some(else_block) = else_block {
+                    self.line("ELSE:");
+                    self.tab(|p| p.visit_statement(else_block));
                 }
-                None => vec![("COMPOUND:".to_string(), deep)],
+
+                self.line("END");
             }
+            Statement::Compound { list } => {
+                if let Some(list) = list {
+                    for block in list {
+                        self.visit_block(block)
+                    }
+                }
+            }
+            Statement::While { exp, statement } => {
+                let exp = self.expr(exp);
+                self.line(&format!("WHILE {}:", exp));
+                self.line("DO");
+                self.tab(|p| p.visit_statement(statement));
+                self.line("END");
+            }
+            Statement::Do { statement, exp } => {
+                let exp = self.expr(exp);
+                self.line(&format!("DO-WHILE {}:", exp));
+                self.line("DO");
+                self.tab(|p| p.visit_statement(statement));
+                self.line("END");
+            }
+            Statement::ForDecl {
+                decl,
+                exp2,
+                exp3,
+                statement,
+            } => {
+                let decl = self.decl(decl);
+                let cond = self.expr(exp2);
+                let exp = exp3.as_ref().map_or("".to_string(), |e| self.expr(e));
+
+                self.line("FOR:");
+                self.line(&format!("decl: {}", decl));
+                self.line(&format!("cond: {}", cond));
+                self.line(&format!("exp: {}", exp));
+                self.line("DO");
+                self.tab(|p| p.visit_statement(statement));
+                self.line("END");
+            }
+            Statement::For {
+                exp1,
+                exp2,
+                exp3,
+                statement,
+            } => {
+                let exp1 = exp1.as_ref().map_or(Default::default(), |e| self.expr(e));
+                let cond = self.expr(exp2);
+                let exp2 = exp3.as_ref().map_or("".to_string(), |e| self.expr(e));
+
+                self.line("FOR:");
+                self.line(&format!("exp: {}", exp1));
+                self.line(&format!("cond: {}", cond));
+                self.line(&format!("exp: {}", exp2));
+                self.line("DO");
+                self.tab(|p| p.visit_statement(statement));
+                self.line("END");
+            }
+            Statement::Break => self.line(&"BREAK".to_owned()),
+            Statement::Continue => self.line(&"CONTINUE".to_owned()),
         }
-        ast::Statement::While { exp, statement } => {
-            let mut out = Vec::new();
-            out.push((format!("WHILE {}:", pretty_expr(exp)), deep));
-            out.push(("DO".to_string(), deep));
-            out.extend(pretty_stat(statement, deep + 1));
-            out.push(("END".to_string(), deep));
-            out
-        }
-        ast::Statement::Do { statement, exp } => {
-            let mut out = Vec::new();
-            out.push((format!("DOWHILE {}:", pretty_expr(exp)), deep));
-            out.push(("DO".to_string(), deep));
-            out.extend(pretty_stat(statement, deep + 1));
-            out.push(("END".to_string(), deep));
-            out
-        }
-        ast::Statement::ForDecl {
-            decl,
-            exp2,
-            exp3,
-            statement,
-        } => {
-            let mut out = Vec::new();
-            out.push(("FOR:".to_string(), deep));
-            out.push((format!("decl: {}", pretty_decl(decl)), deep));
-            out.push((format!("cond: {}", pretty_expr(exp2)), deep));
-            out.push((
-                format!(
-                    "exp: {}",
-                    exp3.as_ref().map_or("".to_string(), |e| pretty_expr(&e))
-                ),
-                deep,
-            ));
-            out.push(("DO".to_string(), deep));
-            out.extend(pretty_stat(statement, deep + 1));
-            out.push(("END".to_string(), deep));
-            out
-        }
-        ast::Statement::For {
-            exp1,
-            exp2,
-            exp3,
-            statement,
-        } => {
-            let mut out = Vec::new();
-            out.push(("FOR:".to_string(), deep));
-            out.push((
-                format!(
-                    "decl: {}",
-                    exp3.as_ref().map_or("".to_string(), |e| pretty_expr(&e))
-                ),
-                deep,
-            ));
-            out.push((format!("cond: {}", pretty_expr(exp2)), deep));
-            out.push((
-                format!(
-                    "exp: {}",
-                    exp3.as_ref().map_or("".to_string(), |e| pretty_expr(&e))
-                ),
-                deep,
-            ));
-            out.push(("DO".to_string(), deep));
-            out.extend(pretty_stat(statement, deep + 1));
-            out.push(("END".to_string(), deep));
-            out
-        }
-        ast::Statement::Break => vec![("BREAK".to_string(), deep)],
-        ast::Statement::Continue => vec![("CONTINUE".to_string(), deep)],
-        _ => unimplemented!(),
     }
-}
 
-fn pretty_decl(st: &ast::Declaration) -> String {
-    match st {
-        ast::Declaration::Declare { name, exp } => match exp {
-            Some(exp) => format!("INT {} = {}", name, pretty_expr(exp)),
-            None => format!("INT {}", name),
-        },
+    fn visit_decl(&mut self, decl: &'a Declaration) {
+        let decl = match decl {
+            Declaration::Declare { name, exp } => match exp {
+                Some(exp) => {
+                    let exp = self.expr(exp);
+                    format!("INT {} = {}", name, exp)
+                }
+                None => format!("INT {}", name),
+            },
+        };
+        self.save(decl);
     }
-}
 
-fn pretty_opt_expr(exp: &Option<ast::Exp>) -> String {
-    exp.as_ref()
-        .map_or("None".to_owned(), |exp| pretty_expr(exp))
-}
-
-fn pretty_expr(exp: &ast::Exp) -> String {
-    match exp {
-        ast::Exp::BinOp(op, exp1, exp2) => format!(
-            "{} BIN_OP<{:?}> {}",
-            pretty_expr(exp1),
-            op,
-            pretty_expr(exp2)
-        ),
-        ast::Exp::Const(c) => format!("{:?}", c),
-        ast::Exp::UnOp(op, exp) => format!("UN_OP<{:?}> {}", op, pretty_expr(exp)),
-        ast::Exp::IncOrDec(name, op) => format!("VAR[{}] {:?}", name, op),
-        ast::Exp::Assign(name, exp) => format!("VAR[{}] = {}", name, pretty_expr(exp)),
-        ast::Exp::Var(name) => format!("VAR[{}]", name),
-        ast::Exp::AssignOp(name, op, exp) => {
-            format!("VAR[{}] ASSIGN_OP<{:?}> {}", name, op, pretty_expr(exp))
-        }
-        ast::Exp::CondExp(cond, exp1, exp2) => format!(
-            "TERNAR_OP IF {} DO {} ELSE DO {}",
-            pretty_expr(cond),
-            pretty_expr(exp1),
-            pretty_expr(exp2)
-        ),
-        ast::Exp::FuncCall(name, params) => format!(
-            "CALL {} WITH {}",
-            name,
-            params
-                .iter()
-                .map(|e| pretty_expr(e))
-                .collect::<Vec<String>>()
-                .join(", ")
-        ),
+    fn visit_block(&mut self, block: &'a BlockItem) {
+        match block {
+            BlockItem::Declaration(decl) => {
+                let decl = self.decl(decl);
+                self.line(&decl);
+            }
+            BlockItem::Statement(st) => self.visit_statement(st),
+        };
     }
 }
--- src/pretty_output/mod.rs
+++ src/pretty_output/mod.rs
@@ -2,6 +2,6 @@ mod decl;
 mod tac;
 mod tokens;
 
-pub use decl::{pretty_func, pretty_prog};
+pub use decl::{pretty_prog};
 pub use tac::pretty as pretty_tac;
 pub use tokens::pretty_tokens;
